3d252a23968ee5c5069fa233ff4498ad2ebf6d44
Luiz Eduardo <luizefm@al.insper.edu.br>
2023-03-30 09:49:43 -0300

    Iniciado a criação do site 'Recipes' com a tecnologia Django.

---
+++ recipes/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Luiz Eduardo',
+    })
